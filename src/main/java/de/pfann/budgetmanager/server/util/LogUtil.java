7f099c2af51f8e6dc079864600b4e9702fe1bb22
Johannes Pfann <jopf88@googlemail.com>
2018-01-18 20:52:15 +0100

    Add new LogUtil

--- src/main/java/de/pfann/budgetmanager/server/util/LogUtil.java
+++ src/main/java/de/pfann/budgetmanager/server/util/LogUtil.java
@@ -8,49 +8,49 @@ public class LogUtil {
 	/*
 	 * error() methods
 	 */
-	public static void error(Class<?> _clazz, String _string) {
-		getLogger(_clazz).error(_string);
+	public static void error(Class<?> aClass, String aString) {
+		getLogger(aClass).error(aString);
 	}
 
-	public static void error(Class<?> _clazz, String _string, Object... _args) {
-		getLogger(_clazz).error(_string, _args);
+	public static void error(Class<?> aClass, String aString, Object... aArgs) {
+		getLogger(aClass).error(aString, aArgs);
 	}
 
 	/*
 	 * warn() methods
 	 */
-	public static void warn(Class<?> _clazz, String _string) {
-		getLogger(_clazz).warn(_string);
+	public static void warn(Class<?> aClass, String aString) {
+		getLogger(aClass).warn(aString);
 	}
 
-	public static void warn(Class<?> _clazz, String _string, Object... _args) {
-		getLogger(_clazz).warn(_string, _args);
+	public static void warn(Class<?> aClass, String aString, Object... aArgs) {
+		getLogger(aClass).warn(aString, aArgs);
 	}
 
 	/*
 	 * info() methods
 	 */
-	public static void info(Class<?> _clazz, String _string) {
-		getLogger(_clazz).info(_string);
+	public static void info(Class<?> aClass, String aString) {
+		getLogger(aClass).info(aString);
 	}
 
-	public static void info(Class<?> _clazz, String _string, Object... _args) {
-		getLogger(_clazz).info(_string, _args);
+	public static void info(Class<?> aClass, String aString, Object... aArgs) {
+		getLogger(aClass).info(aString, aArgs);
 	}
 
 	/*
 	 * debug() methods
 	 */
-	public static void debug(Class<?> _clazz, String _string) {
-		getLogger(_clazz).debug(_string);
+	public static void debug(Class<?> aClass, String aString) {
+		getLogger(aClass).debug(aString);
 	}
 
-	public static void debug(Class<?> _clazz, String _string, Object... _args) {
-		getLogger(_clazz).debug(_string, _args);
+	public static void debug(Class<?> aClass, String aString, Object... aArgs) {
+		getLogger(aClass).debug(aString, aArgs);
 	}
 
-	private static Logger getLogger(Class<?> _clazz) {
-		return LoggerFactory.getLogger(_clazz);
+	private static Logger getLogger(Class<?> aClass) {
+		return LoggerFactory.getLogger(aClass);
 	}
 
 }
