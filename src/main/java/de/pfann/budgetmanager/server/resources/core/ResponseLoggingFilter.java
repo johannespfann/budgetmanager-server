594bbe0fdff23bca11cc3ea78624f962efb8ddcb
Johannes Pfann <jopf88@googlemail.com>
2018-01-20 10:08:17 +0100

    Modified ResponseFilter with new logs

--- src/main/java/de/pfann/budgetmanager/server/resources/core/ResponseLoggingFilter.java
+++ src/main/java/de/pfann/budgetmanager/server/resources/core/ResponseLoggingFilter.java
@@ -16,6 +16,10 @@ public class ResponseLoggingFilter implements ContainerResponseFilter{
     public void filter(ContainerRequestContext aContainerRequestContext, ContainerResponseContext aContainerResponseContext) throws IOException {
         LogUtil.info(this.getClass(),"Filter Response");
         LogUtil.info(this.getClass(), "Status: " + aContainerResponseContext.getStatusInfo().toString());
+        LogUtil.info(this.getClass(), "Entry: " + aContainerResponseContext.getEntity().toString());
+
+
+
     }
 
 }
