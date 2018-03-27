14b407d001186bb446b85148439f754f5e8524a9
johannespfann <johannespfann@users.noreply.github.com>
2018-03-27 09:17:35 +0000

    Update MonthlyRotationEntry.java

--- src/main/java/de/pfann/budgetmanager/server/rotationjobs/MonthlyRotationEntry.java
+++ src/main/java/de/pfann/budgetmanager/server/rotationjobs/MonthlyRotationEntry.java
@@ -22,26 +22,73 @@ public class MonthlyRotationEntry implements RotationEntryPattern {
     }
 
     @Override
-    public boolean isExecutable(LocalDate aToday, RotationEntry aEntry) {
+public boolean isExecutable(LocalDate aToday, RotationEntry aRotationEntry) {
 
-        if(!isValidPattern(aEntry)){
-            return false;
-        }
-
-        // nach beginn
-
-        if(aEntry.getStart_at().after(aToday)){
-            return false;
-        }
-
-        Date lastExecuted = aEntry.getLast_executed();
-
-        LocalDate now = LocalDate.now();
-
-
-
-        return true;
-
-
+    if(!isValidPattern(aRotationEntry)){
+        return false;
     }
+
+    LocalDate startDate = convert(aRotationEntry.getStart_at());
+    LocalDate lastExecuted = null;
+
+    if(aRotationEntry.getLast_executed() != null){
+        lastExecuted = convert(aRotationEntry.getLast_executed());
+    }
+
+    /**
+     * Startzeit muss bereits erreicht sein
+     */
+    if(isBeforStartTime(startDate,aToday)){
+        return false;
+    }
+
+    /**
+     * Wurde noch nie ausgeführt
+     */
+    if(lastExecuted == null){
+        return true;
+    }
+
+    /**
+     * In diesem Monat wurde es bereits ausgeführt
+     */
+    if(isAlreadyExecuted(lastExecuted,aToday)){
+        return false;
+    }
+
+    /**
+     * Heute vor einem oder x-Monate
+     */
+    if(lastExecuted.getDayOfMonth() == aToday.getDayOfMonth()){
+        return true;
+    }
+
+    /**
+     * Wenn heute der 28 ist und letzter groeßer ... wegen Februar
+     */
+    if(lastExecuted.getDayOfMonth() > 28 && aToday.getDayOfMonth() == 28){
+        return true;
+    }
+
+    return false;
+}
+
+private boolean isAlreadyExecuted(LocalDate lastExecuted, LocalDate aToday) {
+    if(lastExecuted.getMonth().equals(aToday.getMonth()) && lastExecuted.getYear() == aToday.getYear()){
+        return false;
+    }
+    return true;
+}
+
+private boolean isValidPattern(RotationEntry aEntry) {
+    return true;
+}
+
+private boolean isBeforStartTime(LocalDate startDate, LocalDate aToday) {
+    return startDate.isAfter(aToday);
+}
+
+private LocalDate convert(Date aDate){
+    return aDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
+}
 }
