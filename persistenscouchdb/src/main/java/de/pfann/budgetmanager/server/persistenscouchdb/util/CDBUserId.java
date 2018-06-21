3cfab2581986429ec181ca7690d70c2aa8b01900
Johannes <jopf88@googlemail.com>
2018-06-21 23:03:39 +0200

    added userid to generate ids for couchdb

---
+++ persistenscouchdb/src/main/java/de/pfann/budgetmanager/server/persistenscouchdb/util/CDBUserId.java
@@ -0,0 +1,58 @@
+package de.pfann.budgetmanager.server.persistenscouchdb.util;
+
+public class CDBUserId {
+
+    public static final String SEPERATOR = ":";
+    public static final String TYP_PREFIX = "user";
+
+    private String username;
+
+    public static CDBUserId create(String aUsername){
+        assertUserIsValid(aUsername);
+        return new CDBUserId(aUsername);
+    }
+
+    public static CDBUserId createWithString(String aValue){
+        String[] values = aValue.split(SEPERATOR);
+        assertPrefixIsValid(values[0]);
+        return new CDBUserId(values[1]);
+    }
+
+    private CDBUserId(String aUsername){
+        username = aUsername;
+    }
+
+
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * getter
+     */
+
+
+
+    public String toString(){
+        return TYP_PREFIX + SEPERATOR + username;
+    }
+
+
+
+
+    /**
+     * asserts
+     */
+
+    private static void assertPrefixIsValid(String value) {
+        if(value == null || !value.equals(TYP_PREFIX)){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void assertUserIsValid(String aUsername) {
+        if(aUsername == null || aUsername.isEmpty()){
+            throw new IllegalArgumentException();
+        }
+    }
+}
