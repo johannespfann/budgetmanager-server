49caa7be8c18e1f50713aaeab54a5e962f5123a4
Johannes <jopf88@googlemail.com>
2018-07-02 23:09:26 +0200

    implemented entryFacade

--- persistenscouchdb/src/main/java/de/pfann/budgetmanager/server/persistenscouchdb/facade/CDBEntryFacade.java
+++ persistenscouchdb/src/main/java/de/pfann/budgetmanager/server/persistenscouchdb/facade/CDBEntryFacade.java
@@ -4,49 +4,147 @@ import de.pfann.budgetmanager.server.common.facade.EntryFacade;
 import de.pfann.budgetmanager.server.common.model.AppUser;
 import de.pfann.budgetmanager.server.common.model.Entry;
 import de.pfann.budgetmanager.server.common.model.Tag;
-import org.ektorp.CouchDbInstance;
-import org.ektorp.impl.ObjectMapperFactory;
+import de.pfann.budgetmanager.server.common.util.DateUtil;
+import de.pfann.budgetmanager.server.persistenscouchdb.dao.CDBEntryDao;
+import de.pfann.budgetmanager.server.persistenscouchdb.dao.CDBEntryDaoFactory;
+import de.pfann.budgetmanager.server.persistenscouchdb.dao.CDBUserDao;
+import de.pfann.budgetmanager.server.persistenscouchdb.dao.CDBUserDaoFactory;
+import de.pfann.budgetmanager.server.persistenscouchdb.model.CDBEntry;
+import de.pfann.budgetmanager.server.persistenscouchdb.model.CDBKonto;
+import de.pfann.budgetmanager.server.persistenscouchdb.model.CDBUser;
+import de.pfann.budgetmanager.server.persistenscouchdb.util.CDBEntryId;
+import de.pfann.budgetmanager.server.persistenscouchdb.util.CDBKontoDatabaseId;
+import de.pfann.budgetmanager.server.persistenscouchdb.util.CDBUserId;
+import de.pfann.budgetmanager.server.persistenscouchdb.util.CDBEntryTransformer;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 public class CDBEntryFacade implements EntryFacade {
 
-    private CouchDbInstance couchDbInstance;
-    private ObjectMapperFactory objectMapperFactory;
+    private CDBEntryDaoFactory entryDaoFactory;
+    private CDBUserDaoFactory userDaoFactory;
 
-    public CDBEntryFacade(CouchDbInstance aCouchInstance, ObjectMapperFactory aObjectMapperFactory){
-        couchDbInstance = aCouchInstance;
-        objectMapperFactory = aObjectMapperFactory;
+    private CDBUserDao userDao;
+
+    public CDBEntryFacade(CDBUserDaoFactory aUserDaoFactory, CDBEntryDaoFactory aEntryDaoFactory){
+        entryDaoFactory = aEntryDaoFactory;
+        userDaoFactory = aUserDaoFactory;
+        userDao = userDaoFactory.createDao();
     }
 
     @Override
     public List<Entry> getEntries(AppUser aUser) {
-        return null;
+        CDBUserId userId = CDBUserId.create(aUser.getName());
+        CDBUser cdbUser = userDao.get(userId.toString());
+
+        CDBKontoDatabaseId kontodbId = CDBKontoDatabaseId.builder()
+                .withKontoHash(cdbUser.getKontos().get(0).getHash())
+                .withUsername(cdbUser.getUsername())
+                .build();
+
+        CDBEntryDao cdbEntryDao = entryDaoFactory.createDao(kontodbId.toString());
+
+        List<CDBEntry> cdbEntries = cdbEntryDao.getAll();
+        List<Entry> entries = new ArrayList<>();
+
+        for(CDBEntry cdbEntry : cdbEntries) {
+            Entry entry = CDBEntryTransformer.createEntry(cdbEntry);
+            entries.add(entry);
+        }
+
+        return entries;
     }
 
     @Override
     public List<Entry> getEntries(Tag aTag) {
-        return null;
+        throw new NotImplementedException();
     }
 
     @Override
     public void persistEntry(Entry aEntry) {
+        CDBUserId userId = CDBUserId.create(aEntry.getAppUser().getName());
+        CDBUser cdbUser = userDao.get(userId.toString());
+        CDBKonto konto = cdbUser.getKontos().get(0);
 
+        /**
+         * find right konto
+         */
+        CDBKontoDatabaseId kontodbId = CDBKontoDatabaseId.builder()
+                .withUsername(cdbUser.getUsername())
+                .withKontoHash(konto.getHash())
+                .build();
+        CDBEntryDao entryDao = entryDaoFactory.createDao(kontodbId.toString());
+
+        CDBEntryId entryId = CDBEntryId.createBuilder()
+                .withHash(aEntry.getHash())
+                .withCreatedAt(DateUtil.asLocalDateTime(aEntry.getCreated_at()))
+                .withKonto(konto.getHash())
+                .withUsername(aEntry.getAppUser().getName())
+                .build();
+
+        CDBEntry cdbEntry = new CDBEntry();
+        cdbEntry.setId(entryId.toString());
+        cdbEntry = CDBEntryTransformer.updateCDBEntry(aEntry,cdbEntry);
+
+        entryDao.add(cdbEntry);
     }
 
     @Override
     public void deleteEntry(Entry aEntry) {
+        CDBUserId userId = CDBUserId.create(aEntry.getAppUser().getName());
+        CDBUser cdbUser = userDao.get(userId.toString());
+        CDBKonto konto = cdbUser.getKontos().get(0);
 
+        CDBKontoDatabaseId kontodbId = CDBKontoDatabaseId.builder()
+                .withUsername(cdbUser.getUsername())
+                .withKontoHash(konto.getHash())
+                .build();
+        CDBEntryDao entryDao = entryDaoFactory.createDao(kontodbId.toString());
+
+        CDBEntryId entryId = CDBEntryId.createBuilder()
+                .withHash(aEntry.getHash())
+                .withCreatedAt(DateUtil.asLocalDateTime(aEntry.getCreated_at()))
+                .withKonto(konto.getHash())
+                .withUsername(aEntry.getAppUser().getName())
+                .build();
+
+        CDBEntry cdbEntry = new CDBEntry();
+        cdbEntry.setId(entryId.toString());
+
+        entryDao.remove(cdbEntry);
     }
 
     @Override
     public Entry getEntry(String aHash) {
-        return null;
+        throw new NotImplementedException();
     }
 
     @Override
     public void update(Entry aEntry) {
+        CDBUserId userId = CDBUserId.create(aEntry.getAppUser().getName());
+        CDBUser cdbUser = userDao.get(userId.toString());
+        CDBKonto konto = cdbUser.getKontos().get(0);
 
+        CDBKontoDatabaseId kontodbId = CDBKontoDatabaseId.builder()
+                .withUsername(cdbUser.getUsername())
+                .withKontoHash(konto.getHash())
+                .build();
+        CDBEntryDao entryDao = entryDaoFactory.createDao(kontodbId.toString());
+
+        CDBEntryId entryId = CDBEntryId.createBuilder()
+                .withHash(aEntry.getHash())
+                .withCreatedAt(DateUtil.asLocalDateTime(aEntry.getCreated_at()))
+                .withKonto(konto.getHash())
+                .withUsername(aEntry.getAppUser().getName())
+                .build();
+
+        CDBEntry cdbEntry = new CDBEntry();
+        cdbEntry.setId(entryId.toString());
+
+        cdbEntry = CDBEntryTransformer.updateCDBEntry(aEntry,cdbEntry);
+
+        entryDao.update(cdbEntry);
     }
 }
