c5014fc9337253d02c5088cad78038b39dc23111
Johannes <jopf88@googlemail.com>
2018-07-07 15:58:13 +0200

    impl mapper for user entry statistics

---
+++ restservices/src/main/java/de/pfann/budgetmanager/server/restservices/resources/util/EntryJsonMapper.java
@@ -0,0 +1,51 @@
+package de.pfann.budgetmanager.server.restservices.resources.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.pfann.budgetmanager.server.common.model.Entry;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class EntryJsonMapper {
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String convertToJson(Entry aEntry){
+        try {
+            return objectMapper.writeValueAsString(aEntry);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "{}";
+    }
+
+    public static String convertToJson(List<Entry> aEntries){
+        try {
+            return objectMapper.writeValueAsString(aEntries);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return "[]";
+    }
+
+    public static Entry convertToEntry(String aValue){
+        try {
+            return objectMapper.readValue(aValue,Entry.class);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static List<Entry> convertToEntries(String aValue){
+        try {
+            return objectMapper.readValue(aValue, List.class );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return new LinkedList<Entry>();
+    }
+
+}
