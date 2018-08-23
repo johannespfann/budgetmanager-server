706fcdb9c86cc3424dc2ab2e6893154074665104
Johannes <jopf88@googlemail.com>
2018-08-23 22:54:19 +0200

    add first classes to backup data

---
+++ persistenscouchdb/src/main/java/de/pfann/budgetmanager/server/persistenscouchdb/file/writer/FileWriter.java
@@ -0,0 +1,64 @@
+package de.pfann.budgetmanager.server.persistenscouchdb.file.writer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.pfann.budgetmanager.server.common.facade.AppUserFacade;
+import de.pfann.budgetmanager.server.common.facade.EntryFacade;
+import de.pfann.budgetmanager.server.common.facade.StandingOrderFacade;
+import de.pfann.budgetmanager.server.common.facade.TagStatisticFacade;
+import de.pfann.budgetmanager.server.common.model.AppUser;
+import de.pfann.budgetmanager.server.common.model.Entry;
+import de.pfann.budgetmanager.server.common.model.StandingOrder;
+import de.pfann.budgetmanager.server.persistenscouchdb.model.CDBUser;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class FileWriter {
+
+    private AppUserFacade userFacade;
+    private EntryFacade entryFacade;
+    private StandingOrderFacade standingOrderFacade;
+    private TagStatisticFacade tagStatisticFacade;
+
+    public FileWriter(AppUserFacade aUserFacade,
+                      EntryFacade aEntryFacade,
+                      StandingOrderFacade aStandingOrderFacade,
+                      TagStatisticFacade aStatisticFacade){
+        userFacade = aUserFacade;
+        entryFacade = aEntryFacade;
+        standingOrderFacade = aStandingOrderFacade;
+        tagStatisticFacade = aStatisticFacade;
+    }
+
+    public void writeUserDataToFile(String aUserName, String aOutputDirektory){
+        AppUser user = userFacade.getUserByNameOrEmail(aUserName);
+
+        // Get all StandingOrder of specific user
+        List<StandingOrder> standingOrders = new LinkedList<>();
+        standingOrders = standingOrderFacade.getRotationEntries(user);
+
+        // Get all entries of specific user
+        List<Entry> entries = new LinkedList<>();
+        entries = entryFacade.getEntries(user);
+
+
+        // Convert StandingOrders to JSON-String
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = "";
+        try {
+            json = objectMapper.writeValueAsString(standingOrders);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(json);
+
+
+
+
+    }
+
+
+
+}
---
+++ persistenscouchdb/src/main/java/de/pfann/budgetmanager/server/persistenscouchdb/file/writer/Starter.java
@@ -0,0 +1,64 @@
+package de.pfann.budgetmanager.server.persistenscouchdb.file.writer;
+
+import de.pfann.budgetmanager.server.common.facade.*;
+import de.pfann.budgetmanager.server.persistenscouchdb.core.CouchDbConnectorFactory;
+import de.pfann.budgetmanager.server.persistenscouchdb.dao.CDBEntryDaoFactory;
+import de.pfann.budgetmanager.server.persistenscouchdb.dao.CDBRunDoaFactory;
+import de.pfann.budgetmanager.server.persistenscouchdb.dao.CDBStandingOrderDaoFactory;
+import de.pfann.budgetmanager.server.persistenscouchdb.dao.CDBUserDaoFactory;
+import de.pfann.budgetmanager.server.persistenscouchdb.facade.*;
+import org.ektorp.CouchDbInstance;
+import org.ektorp.http.HttpClient;
+import org.ektorp.http.StdHttpClient;
+import org.ektorp.impl.ObjectMapperFactory;
+import org.ektorp.impl.StdCouchDbInstance;
+import org.ektorp.impl.StdObjectMapperFactory;
+
+import java.net.MalformedURLException;
+
+public class Starter {
+
+    /*
+server.adress: http://0.0.0.0
+server.port: 8090
+server.basepath: budget
+
+# couchdb
+couchdb.host: http://localhost
+couchdb.port: 5984
+couchdb.prefix: dev-bm
+
+couchdb.pw:
+couchdb.user:
+     */
+
+    public static void main(String[] args) throws MalformedURLException {
+        StdHttpClient.Builder httpClientBuilder = new StdHttpClient.Builder();
+        httpClientBuilder.url("http://localhost:5984");
+
+        HttpClient httpClient = httpClientBuilder.build();
+
+        CouchDbInstance dbInstance = new StdCouchDbInstance(httpClient);
+        ObjectMapperFactory objectMapperFactory = new StdObjectMapperFactory();
+        CouchDbConnectorFactory couchDbConnectorFactory = new CouchDbConnectorFactory(dbInstance,"dev-bm",objectMapperFactory);
+
+        CDBUserDaoFactory userDaoFactory = new CDBUserDaoFactory(couchDbConnectorFactory);
+        CDBKontoDatabaseFacade kontoDatabaseFacade = new CDBKontoDatabaseFacade(couchDbConnectorFactory,dbInstance);
+        CDBEntryDaoFactory entryDaoFactory = new CDBEntryDaoFactory(couchDbConnectorFactory);
+        CDBStandingOrderDaoFactory standingOrderDaoFactory = new CDBStandingOrderDaoFactory(couchDbConnectorFactory);
+        CDBRunDoaFactory runDaoFactory = new CDBRunDoaFactory(couchDbConnectorFactory);
+
+        AppUserFacade userFacade = new CDBUserFacade(userDaoFactory, kontoDatabaseFacade);
+        EntryFacade entryFacade = new CDBEntryFacade(userDaoFactory,entryDaoFactory);
+        StandingOrderFacade standingOrderFacade = new CDBStandingOrderFacade(standingOrderDaoFactory,userDaoFactory);
+        RunFacade runFacade = new CDBRunFacade(runDaoFactory);
+        TagStatisticFacade statisticFacade = new CDBStatisticFacade(userDaoFactory);
+
+        FileWriter writer = new FileWriter(userFacade,entryFacade,standingOrderFacade, statisticFacade);
+
+        writer.writeUserDataToFile("johannes-1234", "");
+
+
+    }
+
+}
