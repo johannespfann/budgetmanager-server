7fe7623167e7a4dff9c9eb839a86b07326bff19e
Johannes <jopf88@googlemail.com>
2018-09-06 18:56:46 +0200

    Fixed RunProvider

--- jobengine/src/main/java/de/pfann/budgetmanager/server/jobengine/core/RunProviderImpl.java
+++ jobengine/src/main/java/de/pfann/budgetmanager/server/jobengine/core/RunProviderImpl.java
@@ -22,8 +22,8 @@ public class RunProviderImpl implements RunProvider {
     public List<Run> prepareRuns(LocalDateTime timeOfLastRun, LocalDateTime timeOfCurrentRun) {
 
         RunLog.info(this.getClass(),"Prepare runs for ");
-        RunLog.info(this.getClass()," TimeOfLastRun    :  " + timeOfLastRun);
-        RunLog.info(this.getClass()," TimeOfCurrentRun :  " + timeOfCurrentRun);
+        RunLog.info(this.getClass(),"TimeOfLastRun    :  " + timeOfLastRun);
+        RunLog.info(this.getClass(),"TimeOfCurrentRun :  " + timeOfCurrentRun);
 
         long durationInMilli = ChronoUnit.MILLIS.between(timeOfLastRun,timeOfCurrentRun);
 
@@ -31,18 +31,29 @@ public class RunProviderImpl implements RunProvider {
 
         List<Run> runs = new ArrayList<>();
         LocalDateTime startTime = timeOfLastRun;
+        RunLog.info(this.getClass(), "StartTime: " + startTime);
 
         for(long index = 0; index < amount; index++){
-            Run run = new Run(startTime.plusSeconds(timeInterval.getTimePerMilliSecond() / 1000));
+            LocalDateTime newStartTime = startTime.plusSeconds(timeInterval.getTimePerMilliSecond() / 1000);
+            Run run = new Run(newStartTime);
             startTime = run.getExecuted_at();
             runs.add(run);
         }
 
-        RunLog.info(this.getClass()," Duraten in Milli  :  " + durationInMilli);
-        RunLog.info(this.getClass()," interval in Milli :  " + timeInterval.getTimePerMilliSecond());
-        RunLog.info(this.getClass()," Duraten in min  :  " + DateUtil.convertMilliSecondsToMinutes(durationInMilli));
-        RunLog.info(this.getClass()," interval in min :  " + DateUtil.convertMilliSecondsToMinutes(timeInterval.getTimePerMilliSecond()));
+        RunLog.info(this.getClass(),"Duraten in Milli  :  " + durationInMilli);
+        RunLog.info(this.getClass(),"interval in Milli :  " + timeInterval.getTimePerMilliSecond());
+        RunLog.info(this.getClass(),"Duraten in min  :  " + DateUtil.convertMilliSecondsToMinutes(durationInMilli));
+        RunLog.info(this.getClass(),"interval in min :  " + DateUtil.convertMilliSecondsToMinutes(timeInterval.getTimePerMilliSecond()));
         RunLog.info(this.getClass(),"dedected " + runs.size() + " runs");
+
+        if(runs.size() != 0){
+            RunLog.info(this.getClass(), "Created following runs:");
+        }
+
+        for(Run run : runs){
+            RunLog.info(this.getClass()," " + run.getExecuted_at());
+        }
+
         return runs;
 
     }
