47687635eb6258db51d6d8497ebe4cf21bd7385f
johannes <jopf88@googlemail.com>
2019-11-10 17:52:01 +0100

    deleted unused readFile method and added new methods to list directory and files

--- persistenscouchdb/src/main/java/de/pfann/budgetmanager/server/persistenscouchdb/file/FileUtil.java
+++ persistenscouchdb/src/main/java/de/pfann/budgetmanager/server/persistenscouchdb/file/FileUtil.java
@@ -1,6 +1,8 @@
 package de.pfann.budgetmanager.server.persistenscouchdb.file;
 
 import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
 public class FileUtil {
@@ -24,19 +26,8 @@ public class FileUtil {
         }
     }
 
-    public static void readFile(String aFileName){
-        File standingOrderFile = new File(aFileName);
-        try {
-            standingOrderFile.createNewFile();
-            FileReader fileReader = new FileReader(standingOrderFile);
-            fileReader.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 
-
-    public static String convertToString(File aFile){
+    public static String readContent(File aFile){
         String content = "";
         try {
             content =  new Scanner(aFile).useDelimiter("\\Z").next();
@@ -44,6 +35,29 @@ public class FileUtil {
             e.printStackTrace();
         }
         return content;
+    }
+
+
+    public static List<File> listDirectories(String aPath) {
+        File baseDirectory = new File(aPath);
+        List<File> directories = new LinkedList<>();
+        for (final File fileEntry : baseDirectory.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                directories.add(fileEntry);
+            }
+        }
+        return directories;
+    }
+
+    public static List<File> listFiles(String aPath) {
+        File baseDirectory = new File(aPath);
+        List<File> files = new LinkedList<>();
+        for (final File fileEntry : baseDirectory.listFiles()) {
+            if (fileEntry.isFile()) {
+                files.add(fileEntry);
+            }
+        }
+        return files;
     }
 
 
